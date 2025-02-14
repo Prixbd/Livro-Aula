754b22ddde7ffe9e69b1cb70750c261a8bc1413a
Prixbd <vanderson.moura@ba.estudante.senai.br>
2025-02-14 22:41:50 +0000

    Aula

---
+++ pasta/models/Livro.java
@@ -0,0 +1,35 @@
+public class Livro {
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+    public void setTitulo(String titulo)
+    {
+        this.titulo = titulo;
+    }
+    public String getAutor()
+    {
+        return autor;
+    }
+    public void setAutor(String autor)
+    {
+        this.autor = autor;
+    }
+    public int getPaginas()
+    {
+        return paginas;
+    }
+    public void setPaginas(int paginas)
+    {
+        this.paginas = paginas;
+    }
+    public double getPreco()
+    {
+        return preco;
+    }
+    public void setPreco(double preco)
+    {
+        this.preco = preco;
+    }
+}
